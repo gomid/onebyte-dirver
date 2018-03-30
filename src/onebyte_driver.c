d22e1c3615919aef61df8f59ef0407d4c1058480
gomid <qwang000@gmail.com>
2018-03-30 21:26:44 +0800

    change to raw_copy_to_user, add header for struct task_struct

--- src/onebyte_driver.c
+++ src/onebyte_driver.c
@@ -6,6 +6,7 @@
 #include <linux/types.h>
 #include <linux/fs.h>
 #include <linux/proc_fs.h>
+#include <linux/sched.h>
 #include <asm/uaccess.h>
 
 #define MAJOR_NUMBER 61
@@ -40,7 +41,7 @@ int onebyte_release(struct inode* inode, struct file* filep)
 ssize_t onebyte_read(struct file* filep, char* buf, size_t count, loff_t* f_pos)
 {   
     // copy data to user space 
-    copy_to_user(buf, onebyte_data, 1);
+    raw_copy_to_user(buf, onebyte_data, 1);
 
     return 1;
 }
@@ -48,7 +49,7 @@ ssize_t onebyte_read(struct file* filep, char* buf, size_t count, loff_t* f_pos)
 ssize_t onebyte_write(struct file* filep, const char* buf, size_t count, loff_t* f_pos)
 {
     // copyt data to kernel space
-    copy_from_user(onebyte_data, buf+count-1, 1);
+    raw_copy_from_user(onebyte_data, buf+count-1, 1);
 
     return 1;
 }
